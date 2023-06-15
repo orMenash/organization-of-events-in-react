a3afa9b3676662db915ecdf3e90bb9ff3139bc45
Or <ormenash@gmail.com>
2023-06-15 22:04:27 +0300

    fix socket chat

--- src/Component/HomePage/eventRouters/ChatObEvent2.jsx
+++ src/Component/HomePage/eventRouters/ChatObEvent2.jsx
@@ -11,32 +11,32 @@ import "./chatOb.css"
 const socketIOClient = io(process.env.REACT_APP_BASIC_URL_SERVER);
 
 export default (props) => {
+  const [cookiesUserName, setcookiesUserName] = useCookies(["UserName"]);
+  const refValue = useRef();
+  const [arrMes, setMgs] = useState([]);
   const navigate = useNavigate();
   const { eventClick } = props;
 
-  useEffect(() => {
-    //const { eventClick } = props;
-    console.log(eventClick , 11111111111111);
 
+  useEffect(() => {
+    getData()
     socketIOClient.on(
-      "live On React",
+      "message",
       (message, UserName, idEvent, timeMass) => {
         if (idEvent === eventClick.NameEvent) {
-          console.log(4444444);
           setMgs((current) => [
             ...current,
             { messages: message, from: UserName, timeMass: timeMass },
           ]);
+
         }
       }
     );
 
+    return () => {
+      socketIOClient.off('message');
+    };
   }, []);
-
-  const [cookiesUserName, setcookiesUserName] = useCookies(["UserName"]);
-
-  const refValue = useRef();
-  const [arrMes, setMgs] = useState([]);
 
   async function getData() {
     if (props.eventClick) {
@@ -44,7 +44,6 @@ export default (props) => {
         `${process.env.REACT_APP_BASIC_URL_SERVER}/event/EventChat`,
         { IdEvent: eventClick.NameEvent }
       );
-      // console.log(status200);
       setMgs(status200.data.TakeIt[0].EventMessages);
     }
   }
@@ -61,15 +60,15 @@ export default (props) => {
         new Date().getMilliseconds()
       )
     );
-    let text = d.toISOString();
+    let time_text = d.toISOString();
+    
     socketIOClient.emit(
-      "live On React",
+      "message",
       refValue.current.value,
       cookiesUserName.UserName,
       eventClick.NameEvent,
-      text
+      time_text
     );
-
 
     const status200 = await axios.post(
       `${process.env.REACT_APP_BASIC_URL_SERVER}/event/EventChat`,
@@ -80,14 +79,11 @@ export default (props) => {
       }
     );
 
-    getData()
+    refValue.current.value = "";
   }
 
-  useEffect(() => {
-    getData();
-  }, []);
-
   if (props.eventClick) {
+
     return (
       <>
         <ol style={{ display: "flex", flexDirection: "column" }} dir="rtl">
@@ -100,7 +96,6 @@ export default (props) => {
           </div>
 
           {arrMes.map((item, index) => {
-            // console.log(props.eventClick.Active == true);
             if (item.from) {
               if (cookiesUserName.UserName == item.from) {
                 return (
@@ -143,7 +138,7 @@ export default (props) => {
             <button
               onClick={() => {
                 sendComm();
-                refValue.current.value = "";
+                
               }}
             >
               תגובה{" "}
